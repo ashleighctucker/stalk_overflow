c179605f7b1806780ec8fe520bd09ed205bfe62c
Patrick Wellman <pwellmancode@gmail.com>
2021-10-18 16:57:08 -0700

    finished for the day

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('hit the router')
   res.render('index', { title: 'a/A Express Skeleton Home' });
 });
 
