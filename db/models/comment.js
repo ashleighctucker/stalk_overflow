aa54cdd4d1dd1e51d520cd8b2736914ad789deaa
Patrick Wellman <pwellmancode@gmail.com>
2021-10-19 13:04:07 -0700

    changed title to content in comments

--- db/models/comment.js
+++ db/models/comment.js
@@ -1,12 +1,28 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const comment = sequelize.define('comment', {
-    title: DataTypes.STRING,
-    answerId: DataTypes.INTEGER,
-    questionId: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER
-  }, {});
-  comment.associate = function(models) {
+  const comment = sequelize.define(
+    "Comment",
+    {
+      content: {
+        allowNull: false,
+        type: DataTypes.STRING(255),
+      },
+      answerID: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      questionId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      userId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+    },
+    {}
+  );
+  comment.associate = function (models) {
     // associations can be defined here
   };
   return comment;
--- db/models/question.js
+++ db/models/question.js
@@ -1,13 +1,32 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const question = sequelize.define('Question', {
-    question: DataTypes.TEXT,
-    title: DataTypes.STRING,
-    questionScore: DataTypes.INTEGER,
-    categoryId: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER
-  }, {});
-  question.associate = function(models) {
+  const question = sequelize.define(
+    "Question",
+    {
+      question: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      title: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      questionScore: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      categoryId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      userId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+    },
+    {}
+  );
+  question.associate = function (models) {
     // associations can be defined here
   };
   return question;
