6265da3adf286b81800c5b8e238b859f200243e5
Sarah Yang <sarayang256@gmail.com>
2021-10-19 16:16:41 -0500

    added styles to main page

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { environment } = require("../config");
 const router = express.Router();
 
 /* GET home page. */
@@ -6,5 +7,11 @@ router.get('/', function (req, res, next) {
   console.log('hit the router');
   res.render('index', { title: 'Stalk Overgrow Home' });
 });
+
+if (environment !== "production") {
+  router.get("/error-test", () => {
+    throw new Error("This is a test error.");
+  });
+}
 
 module.exports = router;
