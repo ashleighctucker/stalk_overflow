63d7a88badfed2b06c0f60b1a6c1b42d39cf5497
Ashleigh Tucker <ashleighctucker@gmail.com>
2021-10-20 10:54:35 -0700

    changed the migration files to work w dynamic seeding

--- db/migrations/20211019191910-create-comment.js
+++ db/migrations/20211019191910-create-comment.js
@@ -39,10 +39,12 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
       },
     });
   },
