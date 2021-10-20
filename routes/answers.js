c73a06adcb3b3ba4af775a351b1944520678aabc
Sarah Yang <sarayang256@gmail.com>
2021-10-20 18:10:23 -0500

    add post to edit and delete answer

--- routes/answers.js
+++ routes/answers.js
@@ -1,17 +1,17 @@
 //External Imports
-const express = require('express');
-const { validationResult } = require('express-validator');
+const express = require("express");
+const { validationResult } = require("express-validator");
 
 //Internal Imports
-const { Answer } = require('../db/models');
-const { asyncHandler, csrfProtection } = require('./utils');
-const { answerValidators } = require('./validators');
+const { Answer } = require("../db/models");
+const { asyncHandler, csrfProtection } = require("./utils");
+const { answerValidators } = require("./validators");
 
 const router = express.Router();
 
 //API endpoint for adding an answer to a question
 router.post(
-  '/',
+  "/",
   csrfProtection,
   answerValidators,
   asyncHandler(async (req, res) => {
@@ -26,13 +26,58 @@ router.post(
       res.redirect(`/questions/${questionId}`);
     } else {
       const errors = validatorErrors.array().map((error) => error.msg);
-      res.render('answer-question', {
-        title: 'Answer',
+      res.render("answer-question", {
+        title: "Answer",
         answer,
         errors,
         csrfToken: req.csrfToken(),
       });
     }
+  })
+);
+
+//* API endpoint for editing an Answer
+router.post(
+  "/edit/:id(\\d+)", // change?
+  answerValidators,
+  asyncHandler(async (req, res) => {
+    const answerId = parseInt(req.params.id, 10);
+    const updateAnswer = await Answer.findByPk(answerId);
+
+    const { answer, title, questionId, userId } = req.body;
+
+    const newAnswer = {
+      answer,
+      title,
+      questionId,
+      userId,
+    };
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      await updateAnswer.update(newAnswer);
+      res.redirect(`/questions/${questionId}`);
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.render("edit-answer", {
+        title: "Edit Answer",
+        answer,
+        errors,
+        csrfToken: req.csrfToken(),
+      });
+    }
+  })
+);
+
+//* API endpoint for deleting a answer
+router.post(
+  '/delete/:id(\\d+)',
+  asyncHandler(async (req, res) => {
+    const answerId = parseInt(req.params.id, 10);
+    const answer = await Answer.findByPk(answerId);
+    await answer.destroy();
+    res.redirect('/');
   })
 );
 
