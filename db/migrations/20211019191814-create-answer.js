782115e1240b5272338ce6ab833ad03ec8aecac6
Ashleigh Tucker <ashleighctucker@gmail.com>
2021-10-20 10:54:35 -0700

    changed the migration files to work w dynamic seeding

--- db/migrations/20211019191814-create-answer.js
+++ db/migrations/20211019191814-create-answer.js
@@ -1,49 +1,64 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Answers', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      title: {
-        allowNull: false,
-        type: Sequelize.STRING(50),
-      },
-      answer: {
-        allowNull: false,
-        type: Sequelize.TEXT,
-      },
-      answerScore: {
-        type: Sequelize.INTEGER,
-      },
-      questionId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'Questions',
-          key: 'id',
+    return queryInterface.createTable(
+      'Answers',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        title: {
+          allowNull: false,
+          type: Sequelize.STRING(50),
+        },
+        answer: {
+          allowNull: false,
+          type: Sequelize.TEXT,
+        },
+        answerScore: {
+          type: Sequelize.INTEGER,
+        },
+        questionId: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+          unique: 'oneAnswer',
+          references: {
+            model: 'Questions',
+            key: 'id',
+          },
+        },
+        userId: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+          unique: 'oneAnswer',
+          references: {
+            model: 'Users',
+            key: 'id',
+          },
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.fn('now'),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.fn('now'),
         },
       },
-      userId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'Users',
-          key: 'id',
+      {
+        // sets up for seeding that we won't have more than one answer per question per user
+        uniqueKeys: {
+          oneAnswer: {
+            fields: ['questionId', 'userId'],
+          },
         },
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
+      }
+    );
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('Answers');
