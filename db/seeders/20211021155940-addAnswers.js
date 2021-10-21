bb01f44187dcd06ced78185a478ada664d475d9f
Patrick Wellman <pwellmancode@gmail.com>
2021-10-21 16:21:20 -0700

    finished dynamically seeding answers

--- db/seeders/20211021155940-addAnswers.js
+++ db/seeders/20211021155940-addAnswers.js
@@ -4,7 +4,7 @@ const { User } = require("../models");
 const { Question } = require("../models");
 
 module.exports = {
-  up: async(queryInterface, Sequelize) => {
+  up: async (queryInterface, Sequelize) => {
     function createAnswers(
       numAnswers,
       numQuestions,
@@ -33,24 +33,30 @@ module.exports = {
           });
           answer = answer.join(" ");
           answerTitle = answerTitle.join("");
+          
+         
+          let questionCount = numQuestions
+          
+          let questionId = (Math.floor(Math.random() * questionCount))
 
-          let questionId =
-            Math.floor(Math.random() * numQuestions) + numQuestions;
-
+        
           while (questionsArray.includes(questionId))
-            questionId =
-              Math.floor(Math.random() * numQuestions) + numQuestions;
+            questionId = Math.floor(Math.random() * questionCount);
 
           questionsArray.push(questionId);
-          console.log("Inside")
+
+
+          
 
           result.push({
             answer,
             userId: i,
             title: answerTitle,
+            questionId,
           });
         }
       }
+      console.log(result);
       return result;
     }
 
@@ -58,19 +64,20 @@ module.exports = {
 
     const numQuestions = await Question.count();
 
-    const numAnswers = Math.floor(Math.random());
+    const numAnswers = 3;
+    console.log(numUsers, numQuestions, numAnswers);
 
     let usersStart = await User.findOne({ order: ["id"] });
     usersStart = usersStart.id + 1;
-
     let answersArray = createAnswers(
       numAnswers,
       numQuestions,
       answers,
       usersStart,
-      numUsers + usersStart - 1,
+      numUsers + usersStart - 1
     );
 
+    console.log(answersArray);
     return queryInterface.bulkInsert("Answers", answersArray, {});
   },
 
