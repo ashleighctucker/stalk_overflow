8fa1811bd413c3c42a8711b9a5896f27330c82e9
Ashleigh Tucker <ashleighctucker@gmail.com>
2021-10-20 19:57:18 -0700

    refactored the post answers route and tested on postman

--- routes/answers.js
+++ routes/answers.js
@@ -1,18 +1,17 @@
 //External Imports
-const express = require("express");
-const { validationResult } = require("express-validator");
+const express = require('express');
+const { validationResult } = require('express-validator');
 
 //Internal Imports
-const { Answer } = require("../db/models");
-const { asyncHandler, csrfProtection } = require("./utils");
-const { answerValidators } = require("./validators");
+const { Answer } = require('../db/models');
+const { asyncHandler, csrfProtection } = require('./utils');
+const { answerValidators } = require('./validators');
 
 const router = express.Router();
 
 //API endpoint for adding an answer to a question
 router.post(
-  "questions/:id(\\d+)/answers",
-  csrfProtection,
+  '/questions/:id(\\d+)/answers',
   answerValidators,
   asyncHandler(async (req, res) => {
     const { title, answer, questionId, userId } = req.body;
@@ -22,12 +21,13 @@ router.post(
 
     if (validatorErrors.isEmpty()) {
       await newAnswer.save();
-      //re routes to specific question page
-      res.redirect(`/questions/${questionId}`);
+      //re routes to specific question page - un-comment and replace once we have that page done
+      // res.redirect(`/questions/${questionId}`);
+      res.redirect(`/`);
     } else {
       const errors = validatorErrors.array().map((error) => error.msg);
-      res.render("answer-question", {
-        title: "Answer",
+      res.render('answer-question', {
+        title: 'Answer',
         answer,
         errors,
         csrfToken: req.csrfToken(),
