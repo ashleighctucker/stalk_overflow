1779530c384cd5b41db5a05eda18acf74ffd64be
Ashleigh Tucker <ashleighctucker@gmail.com>
2021-10-21 10:29:57 -0700

    home page for top questions dynamically seeds questions

--- routes/holdquestions.js
+++ routes/holdquestions.js
@@ -3,10 +3,22 @@ const express = require('express');
 const { validationResult } = require('express-validator');
 
 //Internal Imports
-const { Question } = require('../db/models');
+const { Question, Answer } = require('../db/models');
 const { asyncHandler, csrfProtection } = require('./utils');
 const { questionValidators } = require('./validators');
 
 const router = express.Router();
+
+router.get(
+  '/questions',
+  asyncHandler(async (req, res) => {
+    const questions = await Question.findAll({
+      include: Answer,
+      order: [['updatedAt', 'DESC']],
+      limit: 15,
+    });
+    res.json({ questions });
+  })
+);
 
 module.exports = router;
