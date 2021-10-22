0329f52bd10e04f2c51f3de8a11b026ebcac6e7a
Ashleigh Tucker <ashleighctucker@gmail.com>
2021-10-22 10:22:36 -0700

    fetch updates for hekoku compatibility

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,5 @@
 const fetchQuestions = async () => {
-  const res = await fetch('http://localhost:8080/questions');
+  const res = await fetch('/questions');
   const { questions } = await res.json();
 
   const questionContainer = document.querySelector('.questions-section');
