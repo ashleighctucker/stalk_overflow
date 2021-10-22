12eae21d69b38543ca206947483175419e74c72d
Ashleigh Tucker <ashleighctucker@gmail.com>
2021-10-22 08:48:43 -0700

    replaced after the merge conflict

--- db/seeders/20211021155940-addAnswers.js
+++ db/seeders/20211021155940-addAnswers.js
@@ -1,14 +1,46 @@
 'use strict';
-
-const answers = require("../../answers");
-const { User } = require("../models");
+const answers = require('../../answers');
+const { User } = require('../models');
+const { Question } = require('../models');
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    
+  up: async (queryInterface, Sequelize) => {
+    function createAnswers(
+      numAnswers,
+      numQuestions,
+      answers,
+      usersStart,
+      usersEnd
+    ) {
+      const result = [];
 
+      for (let i = usersStart; i < usersEnd; i++) {
+        let questionsArray = [];
+        for (let j = 0; j < numAnswers; j++) {
+          let answer = [];
+          let answerTitle = [];
 
+          Object.values(answers['answerParts']).forEach((value) => {
+            const arrLength = value.length;
+            const index = Math.floor(Math.random() * arrLength);
+            answer.push(value[index]);
+          });
+          Object.values(answers['titleOptions']).forEach((value) => {
+            const arrLength = value.length;
+            const index = Math.floor(Math.random() * arrLength);
+            answerTitle.push(value[index]);
+          });
+          answer = answer.join(' ');
+          answerTitle = answerTitle.join('');
 
+          let questionCount = numQuestions;
+
+          let questionId = Math.floor(Math.random() * questionCount);
+
+          while (questionsArray.includes(questionId))
+            questionId = Math.floor(Math.random() * questionCount);
+
+          questionsArray.push(questionId);
 
           result.push({
             answer,
@@ -25,12 +57,11 @@ module.exports = {
     const numUsers = await User.count();
 
     const numQuestions = await Question.count();
-    console.log(await Question.count())
+    console.log(await Question.count());
 
     const numAnswers = 3;
 
-
-    let usersStart = await User.findOne({ order: ["id"] });
+    let usersStart = await User.findOne({ order: ['id'] });
     usersStart = usersStart.id + 1;
     let answersArray = createAnswers(
       numAnswers,
@@ -40,19 +71,10 @@ module.exports = {
       numUsers + usersStart - 1
     );
 
-
-    return queryInterface.bulkInsert("Answers", answersArray, {});
-
-
+    return queryInterface.bulkInsert('Answers', answersArray, {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      */
-      return queryInterface.bulkDelete('Answer', null, {});
-  }
+    return queryInterface.bulkDelete('Answers', null, {});
+  },
 };
