045e5ac19064ccb8006e31dbe1665e8268464248
Ashleigh Tucker <ashleighctucker@gmail.com>
2021-10-23 10:40:47 -0700

    finished event listener for delete

--- public/javascripts/questionview.js
+++ public/javascripts/questionview.js
@@ -11,9 +11,21 @@ const createConfirmDeleteButton = (questionId) => {
   return confirmDeleteButton;
 };
 
+const createCancelButton = () => {
+  const cancelButton = document.createElement('button');
+  cancelButton.className = 'login-button';
+  cancelButton.id = 'cancel-delete-button';
+  cancelButton.innerText = 'Cancel';
+  return cancelButton;
+};
+
 deleteQuestionButton.addEventListener('click', async (event) => {
   const url = document.URL.split('/');
   const questionId = url[5];
   const buttonContainer = document.querySelector('.question-edit-buttons');
   const confirmDeleteButton = createConfirmDeleteButton(questionId);
+  const cancelButton = createCancelButton();
+  buttonContainer.innerHTML = ``;
+  buttonContainer.appendChild(confirmDeleteButton);
+  buttonContainer.appendChild(cancelButton);
 });
