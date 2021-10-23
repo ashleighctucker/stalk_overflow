e86dcc55221f75048114079c9b96d5a1759e7ed3
Ashleigh Tucker <ashleighctucker@gmail.com>
2021-10-23 11:43:08 -0700

    refactored delete el

--- public/javascripts/canceledit.js
+++ public/javascripts/canceledit.js
@@ -1,29 +1,32 @@
-const canelButton = document.querySelector('#cancel-delete-button');
+// const canelButton = document.querySelector('#question-button-2');
 
-const createEditQuestionButton = (questionId) => {
-  const editQuestionButton = document.createElement('button');
-  editQuestionButton.id = 'edit-question-button';
-  const editQuestionLink = document.createElement('a');
-  editQuestionLink.href = `/questions/${questionId}/edit`;
-  editQuestionLink.innerText = 'Edit Question';
-  editQuestionButton.appendChild(editQuestionLink);
-  return editQuestionButton;
-};
+// const createEditQuestionButton = (questionId) => {
+//   const editQuestionButton = document.createElement('button');
+//   editQuestionButton.id = 'edit-question-button';
+//   editQuestionButton.className = 'login-button';
+//   const editQuestionLink = document.createElement('a');
+//   editQuestionLink.href = `/questions/${questionId}/edit`;
+//   editQuestionLink.innerText = 'Edit Question';
+//   editQuestionButton.appendChild(editQuestionLink);
+//   return editQuestionButton;
+// };
 
-const createDeleteQuestionButton = () => {
-  const deleteQuestionButton = document.createElement('button');
-  deleteQuestionButton.id = 'delete-question-button';
-  deleteQuestionButton.innerText = 'Delete Question';
-  return deleteQuestionButton;
-};
+// const createDeleteQuestionButton = () => {
+//   const deleteQuestionButton = document.createElement('button');
+//   deleteQuestionButton.id = 'delete-question-button';
+//   deleteQuestionButton.innerText = 'Delete Question';
+//   deleteQuestionButton.className = 'login-button';
+//   return deleteQuestionButton;
+// };
 
-canelButton.addEventListener('click', (event) => {
-  const url = document.URL.split('/');
-  const questionId = url[5];
-  const buttonContainer = document.querySelector('.question-edit-buttons');
-  const editQuestionButton = createEditQuestionButton(questionId);
-  const deleteQuestionButton = createDeleteQuestionButton();
-  buttonContainer.innerHTML = ``;
-  buttonContainer.appendChild(editQuestionButton);
-  buttonContainer.appendChild(deleteQuestionButton);
-});
+// canelButton.addEventListener('click', (event) => {
+
+//   const url = document.URL.split('/');
+//   const questionId = url[5];
+//   const buttonContainer = document.querySelector('.question-edit-buttons');
+//   const editQuestionButton = createEditQuestionButton(questionId);
+//   const deleteQuestionButton = createDeleteQuestionButton();
+//   buttonContainer.innerHTML = ``;
+//   buttonContainer.appendChild(editQuestionButton);
+//   buttonContainer.appendChild(deleteQuestionButton);
+// });
--- public/javascripts/questionview.js
+++ public/javascripts/questionview.js
@@ -1,6 +1,7 @@
-const deleteQuestionButton = document.querySelector('#delete-question-button');
+const deleteQuestionButton = document.querySelector('#question-button-2');
 
 const createConfirmDeleteButton = (questionId) => {
+  const buttonContainer = document.querySelector('#question-button-1');
   const deletePost = document.createElement('form');
   deletePost.action = `/questions/delete/${questionId}`;
   deletePost.method = 'POST';
@@ -9,28 +10,37 @@ const createConfirmDeleteButton = (questionId) => {
   confirmDeleteButton.name = 'confirm-delete-button';
   confirmDeleteButton.className = 'login-button';
   confirmDeleteButton.value = 'Confirm Delete';
+  buttonContainer.innerHTML = ``;
+  buttonContainer.appendChild(deletePost);
   deletePost.appendChild(confirmDeleteButton);
-  return deletePost;
+  return buttonContainer;
 };
 
 const createCancelButton = () => {
+  const buttonContainer = document.querySelector('#question-button-2');
   const cancelButton = document.createElement('button');
   cancelButton.className = 'login-button';
   cancelButton.id = 'cancel-delete-button';
   cancelButton.innerText = 'Cancel';
-  return cancelButton;
+  buttonContainer.innerHTML = ``;
+  buttonContainer.appendChild(cancelButton);
+  return buttonContainer;
 };
 
 deleteQuestionButton.addEventListener('click', (event) => {
-  const url = document.URL.split('/');
-  const questionId = url[5];
-  const buttonContainer = document.querySelector('.question-edit-buttons');
-  const confirmDeleteButton = createConfirmDeleteButton(questionId);
-  const cancelButton = createCancelButton();
-  const formDiv = document.createElement('div');
-  formDiv.className = 'confirm-delete-container';
-  formDiv.appendChild(confirmDeleteButton);
-  formDiv.appendChild(cancelButton);
-  buttonContainer.innerHTML = ``;
-  buttonContainer.appendChild(formDiv);
+  let targetElement = event.target;
+  let selector = '#delete-question-button';
+  if (targetElement.matches(selector)) {
+    const url = document.URL.split('/');
+    const questionId = url[5];
+    const buttonContainer = document.querySelector('.question-edit-buttons');
+    const confirmDeleteButton = createConfirmDeleteButton(questionId);
+    const cancelButton = createCancelButton();
+    const formDiv = document.createElement('div');
+    formDiv.className = 'confirm-delete-container';
+    formDiv.appendChild(confirmDeleteButton);
+    formDiv.appendChild(cancelButton);
+    buttonContainer.innerHTML = ``;
+    buttonContainer.appendChild(formDiv);
+  }
 });
