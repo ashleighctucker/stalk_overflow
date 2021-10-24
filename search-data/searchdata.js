24cf8878a7f625ed6399967792a7700a16f388db
Ashleigh Tucker <ashleighctucker@gmail.com>
2021-10-23 21:04:26 -0700

    fixed search

--- search-data/searchdata.js
+++ search-data/searchdata.js
@@ -1,73 +1,30 @@
-// //External Imports
-// const express = require("express");
+const { Answer, Category, Question, User } = require('../db/models');
 
-//Internal Imports
-const { Answer, Category, Question, User } = require("../db/models");
-
-// const { csrfProtection, asyncHandler } = require("../utils");
-
-// const Sequelize = require("sequelize");
-const { Op } = require("sequelize");
-// const router = express.Router();
-// ================= SEARCH =========================
+const { Op } = require('sequelize');
 
 async function searchQuestions(term) {
   // search for questions with the given term in its title
-  return await Question.findAll({
+  const questionsTitle = await Question.findAll({
     where: {
-    //   title: { [Op.like]: term }, // if you want "term" in a title
-      question: { [Op.like]: term },
+      title: { [Op.iLike]: `%${term}%` },
     },
-
-
-    });
-        //   const questions = await Question.findAll(
-        //     // include: [{ model: Quest, as: "user", attributes: ["username"] }],
-        //     // order: [["createdAt", "DESC"]],
-        //     // attributes: ["question"],
-
-        //   );
-
-    // return questions;
+    limit: 15,
+  });
+  let questions = new Set(questionsTitle);
+  const questionsQuestion = await Question.findAll({
+    where: {
+      question: { [Op.iLike]: `%${term}%` },
+    },
+    limit: 15,
+  });
+  questionsQuestion.forEach((question) => {
+    if (!questions.has(question)) {
+      questions.add(question);
+    }
+  });
+  return Array.from(questions);
 }
-
-// async function getTenNewestQuestions() {
-//   return await Question.findAll({
-//     limit: 10,
-//     order: [["updatedAt", "DESC"]],
-//   });
-// }
-
-// async function findAnswer(term) {
-//   return await Answer.findAll({
-//     where: {
-//       title: { [Op.like]: term },
-//       answer: { [Op.like]: term },
-//     },
-//   });
-// }
-
-// async function findUser() {
-//   return await User.findAll({ include: [User] });
-// }
-
-// async function findByCategory(term) {
-//      return await Category.findAll({ where: { title: { [Op.like]: term } } });
-// }
-
-// ===================================================
-// Case insensitive search on backend
-
-// const films = await db.Film.findAll({
-//   where: {
-//     title: {
-//       [Sequelize.Op.iLike]: search
-//     }
-//   }
-// })
-// ==================================================
 
 module.exports = {
   searchQuestions,
-//   findAnswer,
 };
